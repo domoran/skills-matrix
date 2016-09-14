6bbda0c9138d1ee987077f0b89fa3675646d7f35
Mathias Mamsch <mmamsch@googlemail.com>
2016-09-14 12:03:32 +0200

    Complete build script

--- build.bat
+++ build.bat
@@ -1,7 +1,24 @@
 PUSHD %~dp0
 set BUILDDIR=%CD%\..\skills-matrix-build
+RD /Q /S "%BUILDDIR%"
 if NOT EXIST "%BUILDDIR%" mkdir "%BUILDDIR%"
 
-meteor build --architecture os.windows.x86_32 "%BUILDDIR%"
+call meteor build --architecture os.windows.x86_32 "%BUILDDIR%"
 
 popd
+
+pushd %BUILDDIR%
+
+tar xvf skills-matrix.tar.gz
+
+cd bundle\programs\server
+
+call npm install
+
+popd 
+
+pushd %BUILDDIR%
+
+tar -czf skill-matrix-winx86.tar.gz bundle
+
+popd 
